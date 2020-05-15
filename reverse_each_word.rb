af0c2661dd71965dbb1102993bc88e8825c026d8
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 09:52:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 
-def reverse_each_word(array)
-  new_array = array.split(" ")
+def reverse_each_word(sentences)
+  new_array = sentences.split(" ")
   new_array.collect {|x| x.reverse!}
   new_array.join(" ")
-  #reversed_array = new_array.each {|x| x.reverse!}
-  #return reversed_array.join(" ")
 end
 
  
